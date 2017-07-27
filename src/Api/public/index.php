7b87696405c835271eab32a0648e8c2102d4518b
SimonIsMe <szym1991@gmail.com>
2017-07-27 07:20:20 +0200

    "user" instead of "login" - keys in the config array

--- src/Api/public/index.php
+++ src/Api/public/index.php
@@ -10,14 +10,14 @@ $applicationConfig = new \Api\ApplicationConfig(
 		'rabbitmq' => [
 			'host' => urlencode(getenv('RABBIT_HOST')),
 			'port' => (int) urlencode(getenv('RABBIT_PORT')),
-			'login' => urlencode(getenv('RABBIT_LOGIN')),
+			'user' => urlencode(getenv('RABBIT_LOGIN')),
 			'password' => urlencode(getenv('RABBIT_PASSWORD')),
 			'channel' => urlencode(getenv('RABBIT_CHANNEL')),
 		],
 		'mysql' => [
 			'host' => urlencode(getenv('MYSQL_HOST')),
 			'port' => (int) urlencode(getenv('MYSQL_PORT')),
-			'login' => urlencode(getenv('MYSQL_LOGIN')),
+			'user' => urlencode(getenv('MYSQL_LOGIN')),
 			'password' => urlencode(getenv('MYSQL_PASSWORD')),
 			'database' => urlencode(getenv('MYSQL_DATABASE')),
 		],
