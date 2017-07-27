72a75cb4736021b7dc768c2455b1960a2ed9253c
SimonIsMe <szym1991@gmail.com>
2017-07-27 20:38:38 +0200

    Debug in the installation process

--- install.php
+++ install.php
@@ -1,12 +1,18 @@
 <?php
 
-echo "\n\nStart installation";
+echo "\n\nStart installation\n\n";
 
 $host = getenv('MYSQL_HOST');
 $port = (int) getenv('MYSQL_PORT');
 $user = getenv('MYSQL_LOGIN');
 $password = getenv('MYSQL_PASSWORD');
 $database = getenv('MYSQL_DATABASE');
+
+echo " - Host: " + $host + "\n";
+echo " - Port: " + $port + "\n";
+echo " - User: " + $user + "\n";
+echo " - Password: " + $password + "\n";
+echo " - Database: " + $database + "\n";
 
 $mysqli = new mysqli(
 	$host,
@@ -17,6 +23,16 @@ $mysqli = new mysqli(
 );
 
 
+if ($mysqli->connect_errno) {
+	echo "Can not connect to the database";
+	exit(1);
+}
+
+if ($mysqli->ping() === false) {
+	echo "Can not send a ping to the database";
+	exit(1);
+}
+
 $dirPath = __DIR__ . '/migrations';
 foreach (scandir($dirPath) as $fileName) {
 	if ($fileName === '.' || $fileName === '..') {
@@ -26,5 +42,5 @@ foreach (scandir($dirPath) as $fileName) {
 	$mysqli->multi_query(file_get_contents($dirPath . $fileName));
 }
 
-echo "Installation finished successfully\n\n";
+echo "\n\nInstallation finished successfully\n\n";
 
