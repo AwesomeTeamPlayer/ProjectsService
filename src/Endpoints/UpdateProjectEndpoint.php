c64185886da0cff074c58ac16c4c15be8a3a3c85
SimonIsMe <szym1991@gmail.com>
2017-09-12 22:21:09 +0200

    [v-2.0.0] Create UpdateProjectEndpoint

---
+++ src/Endpoints/UpdateProjectEndpoint.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace Endpoints;
+
+use Adapters\ProjectsRepositoryInterface;
+use Adapters\ProjectsUsersRepositoryInterface;
+use Carbon\Carbon;
+use Domain\EventSender;
+use Domain\ValueObjects\Project;
+use Validator\ArrayValidator;
+use Validator\IsIntegerValidator;
+use Validator\IsNotNullValidator;
+use Validator\IsStringValidator;
+use Validator\StringLengthValidator;
+use Validator\ValidationResult;
+use Validator\ValidatorsCollection;
+use Validators\ProjectIdExistsValidator;
+
+class UpdateProjectEndpoint extends AbstractEndpoint
+{
+	/**
+	 * @var ProjectsUsersRepositoryInterface
+	 */
+	private $projectsUsersRepository;
+
+	/**
+	 * @var ProjectsRepositoryInterface
+	 */
+	private $projectsRepository;
+
+	/**
+	 * @var EventSender
+	 */
+	private $eventSender;
+
+	public function __construct(
+		ProjectsUsersRepositoryInterface $projectsUsersRepository,
+		ProjectsRepositoryInterface $projectsRepository,
+		EventSender $eventSender
+	)
+	{
+		$this->projectsUsersRepository = $projectsUsersRepository;
+		$this->projectsRepository = $projectsRepository;
+		$this->eventSender = $eventSender;
+	}
+
+	protected function validate(array $data): ValidationResult
+	{
+		$validators = [
+			'projectId' => new ValidatorsCollection([
+				new IsNotNullValidator(),
+				new IsStringValidator(),
+				new StringLengthValidator(10, 10),
+				new ProjectIdExistsValidator($this->projectsRepository)
+			]),
+			'name' => new ValidatorsCollection([
+				new IsNotNullValidator(),
+				new IsStringValidator(),
+				new StringLengthValidator(1, 200)
+			]),
+			'type' => new ValidatorsCollection([
+				new IsNotNullValidator(),
+				new IsIntegerValidator(),
+			]),
+//			'userIds' => new ValidatorsCollection([
+//				new IsNotNullValidator(),
+//				//  todo: check if all user ID are strings with 10 characters
+//			])
+		];
+		$arrayValidator = new ArrayValidator();
+		return $arrayValidator->validateArray($validators, $data);
+	}
+
+	protected function run(array $data)
+	{
+		$project = new Project(
+			$data['projectId'],
+			$data['name'],
+			$data['type'],
+			false,
+			Carbon::now()
+		);
+		$this->projectsRepository->update($project);
+		$this->eventSender->sendProjectCreatedEvent($project);
+
+		$perPage = 100;
+		$count = $this->projectsUsersRepository->countUsers($project->getId());
+		$pages = ceil($count / $perPage);
+
+		$userIdsHaveToBe = $data['userIds'];
+		for ($page = 0; $page < $pages; $page++) {
+			$saved = $this->projectsUsersRepository->getOrderedUsersByProjectId(
+				$project->getId(),
+				$page * $perPage,
+				$perPage
+			);
+
+			$toRemove = array_diff($saved, $userIdsHaveToBe);
+			$userIdsHaveToBe = array_diff($userIdsHaveToBe, $saved);
+
+			foreach ($toRemove as $userIdToRemove) {
+				$this->projectsUsersRepository->removeUser($userIdToRemove, $project->getId());
+				$this->eventSender->sendUserFromProjectRemovedEvent($project->getId(), $userIdToRemove);
+			}
+		}
+
+		foreach ($userIdsHaveToBe as $userId) {
+			$this->projectsUsersRepository->addUser($userId, $project->getId());
+			$this->eventSender->sendUserToProjectAddedEvent($project->getId(), $userId);
+		}
+
+		return $project->getId();
+	}
+}
---
+++ tests/end-to-end/UpdateProjectEndpointsTest.php
@@ -0,0 +1,157 @@
+<?php
+
+use Adapters\MysqlProjectsRepository;
+use Adapters\MysqlProjectsUsersRepository;
+use Domain\EventSender;
+use Endpoints\CreateProjectEndpoint;
+use Endpoints\InvalidDataException;
+use Endpoints\UpdateProjectEndpoint;
+
+class UpdateProjectEndpointsTest extends AbstractEndToEndTest
+{
+	/**
+	 * @var MysqlProjectsRepository
+	 */
+	private $mysqlProjectsRepository;
+
+	/**
+	 * @var MysqlProjectsUsersRepository
+	 */
+	private $mysqlProjectsUsersRepository;
+
+	/**
+	 * @var CreateProjectEndpoint
+	 */
+	private $createProjectEndpoint;
+
+	public function setUp()
+	{
+		parent::setUp();
+		$this->mysqlProjectsRepository = new MysqlProjectsRepository($this->mysqli);
+		$this->mysqlProjectsUsersRepository = new MysqlProjectsUsersRepository($this->mysqli);
+		$this->createProjectEndpoint = new UpdateProjectEndpoint(
+			$this->mysqlProjectsUsersRepository,
+			$this->mysqlProjectsRepository,
+			new EventSender($this->channel)
+		);
+	}
+
+	/**
+	 * @dataProvider dataProvider_test_with_incorrect_data
+	 */
+	public function test_with_incorrect_data($data, $expectedException)
+	{
+		try {
+			$this->createProjectEndpoint->execute($data);
+		} catch (InvalidDataException $exception) {
+			$this->assertEquals($exception->getErrorTexts(), $expectedException);
+		}
+	}
+
+	public function dataProvider_test_with_incorrect_data()
+	{
+		return [
+			[
+				[],
+				[
+					'projectId' => 'This value does not exist.',
+					'name' => 'This value does not exist.',
+					'type' => 'This value does not exist.'
+				],
+			],
+			[
+				[
+					'name' => ''
+				],
+				[
+					'projectId' => 'This value does not exist.',
+					'name' => [
+						'StringLengthValidator' => 'Given string has to have length in given inclusive range <1, 200>.'
+					],
+					'type' => 'This value does not exist.'
+				],
+			],
+			[
+				[
+					'projectId' => '123',
+					'name' => '',
+					'type' => 'aaa'
+				],
+				[
+					'projectId' => [
+						'StringLengthValidator' => 'Given string has to have length in given inclusive range <10, 10>.',
+					],
+					'name' => [
+						'StringLengthValidator' => 'Given string has to have length in given inclusive range <1, 200>.'
+					],
+					'type' => [
+						'IsIntegerValidator' => 'Given value has to be an integer.'
+					]
+				],
+			],
+		];
+	}
+
+	public function test_when_project_does_not_exists()
+	{
+		try {
+			$this->createProjectEndpoint->execute([
+				'projectId' => '1234567890',
+				'name' => 'Project Name',
+				'type' => 123,
+				'userIds' => []
+			]);
+		} catch (InvalidDataException $exception) {
+			$this->assertEquals($exception->getErrorTexts(), [
+				'projectId' => [
+					'ProjectIdExistsValidator' => 'Project does not exist.',
+				]
+			]);
+		}
+	}
+
+//	public function test_correct_with_users()
+//	{
+//		$projectId = $this->createProjectEndpoint->execute([
+//			'name' => 'Project Name',
+//			'type' => 123,
+//			'userIds' => ['user_1', 'user_2']
+//		]);
+//
+//		$project = $this->mysqlProjectsRepository->getProject($projectId);
+//
+//		$this->assertEquals($projectId, $project->getId());
+//		$this->assertEquals('Project Name', $project->getName());
+//		$this->assertEquals(123, $project->getType());
+//
+//		$userIds = $this->mysqlProjectsUsersRepository->getUsersByProjectId($projectId);
+//		$this->assertEquals(['user_1', 'user_2'], $userIds);
+//
+//		$message = $this->channel->basic_get(AbstractEndToEndTest::QUEUE_NAME, true);
+//		$this->assertEquals('project.created', $message->delivery_info['routing_key']);
+//		$this->assertEquals('project.created', json_decode($message->getBody(), true)['name']);
+//		$this->assertEquals([
+//			'name' => 'Project Name',
+//			'type' => 123,
+//		], array_diff_key(json_decode($message->getBody(), true)['data'], ['projectId' => '', 'createdAt' => '']));
+//		$this->assertEquals([
+//			'projectId', 'name', 'type', 'createdAt'
+//		], array_keys(json_decode($message->getBody(), true)['data'] ));
+//
+//		$message = $this->channel->basic_get(AbstractEndToEndTest::QUEUE_NAME, true);
+//		$this->assertEquals('project.user.added', $message->delivery_info['routing_key']);
+//		$this->assertEquals('project.user.added', json_decode($message->getBody(), true)['name']);
+//		$this->assertEquals('user_1', json_decode($message->getBody(), true)['data']['userId']);
+//		$this->assertEquals([
+//			'projectId', 'userId'
+//		], array_keys(json_decode($message->getBody(), true)['data'] ));
+//
+//		$message = $this->channel->basic_get(AbstractEndToEndTest::QUEUE_NAME, true);
+//		$this->assertEquals('project.user.added', $message->delivery_info['routing_key']);
+//		$this->assertEquals('project.user.added', json_decode($message->getBody(), true)['name']);
+//		$this->assertEquals('user_2', json_decode($message->getBody(), true)['data']['userId']);
+//		$this->assertEquals([
+//			'projectId', 'userId'
+//		], array_keys(json_decode($message->getBody(), true)['data'] ));
+//	}
+}
